6b6e2c3f631b1dfe8d5db9a883984f79341c9467
Michal Wlodarczyk <michalwlodarczykbusiness@gmail.com>
2022-12-28 23:35:38 +0100

    add edit

--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -3,9 +3,55 @@
     <input type="text" v-model="searchText" placeholder="Search by email" />
     <button @click="searchUser" class="search-button">Search</button>
     <button @click="getUsers" class="modern-button">Reload Users</button>
-    <button @click="addUser" class="add-button">Add User</button>
+    <button @click="showAddUserForm = true" class="add-button">Add User</button>
+    <div v-if="showAddUserForm" class="form-widget" >
+        <!--submit prevent zapobiega defaultowemu wysłaniu formy i zrealoadowaniu strony po submicie,
+            dzieki temu mozna najpierw wypelnic forme i potem po stronie klienta wyslac
+            wypelnione dane jako nowego uzytkownika
+         -->
+        <form @submit.prevent="addUser">
+            <label for="firstName">First Name:</label>
+            <input v-model="firstName" type="text" id="firstName" required  class="form-element">
+            <br>
+            <label for="lastName">Last Name:</label>
+            <input v-model="lastName" type="text" id="lastName" required  class="form-element">
+            <br>
+            <label for="email">Email:</label>
+            <input v-model="email" type="email" id="email" required  class="form-element">
+            <br>
+            <label for="age">Age:</label>
+            <input v-model="age" type="number" id="age" required  class="form-element">
+            <br>
+            <button type="submit" class="add-button" >Add User</button>
+            <button @click="showAddUserForm = false" class="add-button">Cancel</button>
+        </form>
+    </div>
+
+    <div v-if="showEditUserForm" class="form-widget" >
+        <!--submit prevent zapobiega defaultowemu wysłaniu formy i zrealoadowaniu strony po submicie,
+            dzieki temu mozna najpierw wypelnic forme i potem po stronie klienta wyslac
+            wypelnione dane jako nowego uzytkownika
+         -->
+        <form @submit.prevent="editUser">
+            <label for="firstName">First Name:</label>
+            <input v-model="currentFirstName" type="text" id="firstName" required  class="form-element">
+            <br>
+            <label for="lastName">Last Name:</label>
+            <input v-model="currentLastName" type="text" id="lastName" required  class="form-element">
+            <br>
+            <label for="email">Email:</label>
+            <input v-model="currentEmail" type="email" id="email" required  class="form-element">
+            <br>
+            <label for="age">Age:</label>
+            <input v-model="currentAge" type="number" id="age" required  class="form-element">
+            <br>
+            <button type="submit" @click="editUser(currentId)" class="add-button" >Update</button>
+            <button @click="showEditUserForm = false" class="add-button">Cancel</button>
+        </form>
+    </div>
+
     <table class="centered">
-        <tr class="centered">
+        <tr class="centered"> 
           <th>ID</th>
           <th>First Name</th>
           <th>Last Name</th>
@@ -20,6 +66,12 @@
           <td>{{ user.age}}</td>
           <td>
             <button @click="deleteUser(user.id)" class="delete-button">Delete</button>
+            <button @click="showEditUserForm = true, currentId = user.id,
+            currentFirstName = user.firstName,
+            currentLastName = user.lastName,
+            currentAge = user.age,
+            currentEmail = user.email"
+             class="edit-button">Edit</button>
           </td>
         </tr>
       </table>
@@ -35,7 +87,18 @@ import axios from 'axios'
                 users: [],
                 dataFetched: false,
                 searchText: '',
-                filteredUsers: []
+                filteredUsers: [],
+                showAddUserForm: false,
+                showEditUserForm: false,
+                currentId: -1,
+                currentFirstName: '',
+                currentLastName: '',
+                currentEmail: '',
+                currentAge: 0,
+                firstName: '',
+                lastName: '',
+                email: '',
+                age: '',
             }
         },
         created() {
@@ -75,7 +138,44 @@ import axios from 'axios'
                 console.log(this.filteredUsers)
             },
             addUser(){
-
+                axios.post('http://localhost:8083/api/clients', {
+                firstName: this.firstName,
+                lastName: this.lastName,
+                email: this.email,
+                age: this.age,
+                })
+                .then((response) => {
+                    console.log(response.data);
+                    this.showAddUserForm = false;
+                    this.firstName = '';
+                    this.lastName = '';
+                    this.email = '';
+                    this.age = '';
+                    this.getUsers();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+            },
+            editUser(id){
+                axios.put(`http://localhost:8083/api/clients/${id}`, {
+                firstName: this.currentFirstName,
+                lastName: this.currentLastName,
+                email: this.currentEmail,
+                age: this.currentAge,
+                })
+                .then((response) => {
+                    console.log(response.data);
+                    this.showEditUserForm = false;
+                    this.currentFirstName = '';
+                    this.currentLastName = '';
+                    this.currentEmail = '';
+                    this.currentAge = 0;
+                    this.getUsers();
+                })
+                .catch(error => {
+                    console.log(error);
+                });
             }
         }
     }
@@ -119,7 +219,7 @@ import axios from 'axios'
   padding: 10px 20px;
   border: none;
   border-radius: 4px;
-  background-color: red;
+  background-color: #ff4557;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   transition: all 0.1s ease-in-out;
   margin: 20px;
@@ -168,10 +268,40 @@ input[type="text"] {
   border-radius: 4px;
 }
 
+input[type="email"] {
+  padding: 16px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  transition: all 0.2s ease-in-out;
+  border-radius: 4px;
+}
+
+
+input[type="number"] {
+  padding: 16px;
+  border: 1px solid #ccc;
+  background-color: #fff;
+  transition: all 0.2s ease-in-out;
+  border-radius: 4px;
+}
+
+
 input[type="text"]:focus {
   border: 1px solid #4caf50;
   box-shadow: 0 0 10px #4caf50;
 }
+
+input[type="email"]:focus {
+  border: 1px solid #4caf50;
+  box-shadow: 0 0 10px #4caf50;
+}
+
+
+input[type="number"]:focus {
+  border: 1px solid #4caf50;
+  box-shadow: 0 0 10px #4caf50;
+}
+
 
 .search-button {
   display: inline-block;
@@ -224,4 +354,56 @@ input[type="text"]:focus {
   transform: translateY(0);
   box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
 }
+
+.form-widget {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%; 
+  margin: 0 auto;
+}
+
+form {
+  background: white;
+  border-radius: 4px;
+  padding: 20px;
+}
+
+.form-element {
+  width: 80%;
+  margin: 0 auto;
+}
+
+.edit-button {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: 500;
+  color: #fff;
+  text-align: center;
+  text-decoration: none;
+  padding: 10px 15px;
+  border: none;
+  border-radius: 4px;
+  background-color: #e8d335;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  transition: all 0.1s ease-in-out;
+  margin: 20px;
+}
+
+.edit-button:hover {
+  transform: translateY(-2px);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.edit-button:active {
+  transform: translateY(0);
+  box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+}
+
 </style>
